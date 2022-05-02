f23108daceb405a632bf9e363f8b9fee59107e04
Sara Williams <sara.williams.n@gmail.com>
2022-05-02 14:52:10 -0400

    learned useState to set initial state and change the state

--- src/App.js
+++ src/App.js
@@ -1,68 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-//import leif from "./leif.jpg";
-
-/*function Header (props) {
-  return (
-    <h1>{props.name}'s Kitchen</h1>
-  );
-}
-
-function Main (props) {
-  return (
-    <section>
-      <p>We serve the most delicious food around</p>
-      <img 
-        src={leif} 
-        height={200} 
-        alt="a Great Pyrenees stands in a snowy yard"
-      />
-      <ul style={{textAlign: "left"}}>
-        {props.dishes.map((dish)=> (
-          <li key={dish.id}>{dish.title}</li>
-        ))}
-      </ul>
-    </section>
-  );
-}
-
-function Footer (props) {
-  return(
-    <h3>Copyright {props.year}  |  sara.williams.n@gmail.com  |  (970) 412-4699  |  GitHub</h3>
-  );
-}
-
-//How to make an array
-const dishes = [
-  "Macaroni and Cheese",
-  "Salmon",
-  "Tofu with Vegetables",
-  "Misir Wat",
-  "Tuna Rice Bowls"
-];
-
-//Transform array into object that has dishes in it, with each one having a key
-const dishObject = dishes.map((dish, i) => ({id: i, title: dish}));*/
-
-function SecretComponent() {
-  return (
-    <h1>Secret information for authorized users only</h1>
-  )
-}
-
-function RegularComponent() {
-  return (
-    <h1>Everyone can see this component.</h1>
-  )
-}
 
 
-function App({authorized}) {
+function App() {
+  const [emotion, setEmotion] = useState("happy");
+  console.log(emotion);
   return (
     <>
-      {authorized ? <SecretComponent /> : <RegularComponent /> }
+     <h1>Current emotion is {emotion}</h1>
+     <button onClick={()=> setEmotion("happy")}>
+       Happy
+      </button>
+     <button onClick={()=> setEmotion("frustrated")}>
+       Frustrate
+      </button>
+      <button onClick={()=> setEmotion("enthusiastic")}>
+        Enthuse
+      </button>
     </>
-  )
+  );
 }
 
 export default App;
