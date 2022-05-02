7096c9cd1fd87825d0e9e2b5fdb5ad5120d1b23c
Sara Williams <sara.williams.n@gmail.com>
2022-05-02 15:06:50 -0400

    learned useEffect and dependency arrays to use secondary effects

--- src/App.js
+++ src/App.js
@@ -4,18 +4,30 @@ import './App.css';
 
 function App() {
   const [emotion, setEmotion] = useState("happy");
-  console.log(emotion);
+  const [secondary, setSecondary] = useState("tired");
+  
+  useEffect(()=> {
+    console.log(`It's ${emotion} around here!`);
+  },[emotion]);
+
+  useEffect(()=> {
+    console.log(`It's ${secondary} around here!`)
+  },[secondary]);
+
   return (
     <>
-     <h1>Current emotion is {emotion}</h1>
+     <h1>Current emotion is {emotion} and {secondary}</h1>
      <button onClick={()=> setEmotion("happy")}>
-       Happy
+       Make Happy
       </button>
      <button onClick={()=> setEmotion("frustrated")}>
        Frustrate
       </button>
       <button onClick={()=> setEmotion("enthusiastic")}>
         Enthuse
+      </button>
+      <button onClick={()=> setSecondary("crabby")}>
+        Make Crabby
       </button>
     </>
   );
