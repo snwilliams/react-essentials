6b0aaba957d83d2e15b6e85924a42d14d13fd08c
Sara Williams <sara.williams.n@gmail.com>
2022-05-03 15:52:54 -0400

    added states to handle loading and errors

--- src/App.js
+++ src/App.js
@@ -5,27 +5,33 @@ import './App.css';
 
 function App({ login }) {
   const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-useEffect(() => {
-  fetch(`https://api.github.com/users/${login}`)
-    .then(response => response.json())
-    .then(setData);
-}, []);
+  useEffect(() => {
+    if (!login) {
+      return;
+    }
+    setLoading(true);
+    fetch(`https://api.github.com/users/${login}`)
+      .then(response => response.json())
+      .then(setData)
+      .then(setLoading(false))
+      .catch(setError)
+  }, [login]);
 
-if (data) {
+  if(loading) return <h1>Loading...</h1>;
+  if(error) 
+    return <pre>{JSON.stringify(error, null, 2)}</pre>;
+  if(!data) return null;
+
+
   return (
     <div>
       <h1>{data.name}</h1>
       <p>{data.url}</p>
       <img alt={data.login} src={data.avatar_url} />
     </div>
-  );
-}
-
-  return (
-   <div>
-     No user available.
-   </div>
   );
 }
 
