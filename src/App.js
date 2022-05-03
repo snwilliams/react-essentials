3271c38629a25c4c56bdd16bb40090a5c9ea3fa6
Sara Williams <sara.williams.n@gmail.com>
2022-05-03 15:33:39 -0400

    used useState and useEffect to make a call to the GitHub API

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import React, { useReducer } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+//https://api.github.com/users/snwilliams
 
-function App() {
-  const [checked, toggle] = useReducer(
-      (checked) => !checked,
-      false
-  );
+function App({ login }) {
+  const [data, setData] = useState(null);
+
+useEffect(() => {
+  fetch(`https://api.github.com/users/${login}`)
+    .then(response => response.json())
+    .then(setData);
+}, []);
+
+if (data) {
+  return <div>{JSON.stringify(data)}</div>
+}
 
   return (
-    <>
-      <input 
-      type="checkbox" 
-      value={checked} 
-      onChange={toggle}
-      />
-     <p>{checked ? "checked" : "not checked"}</p>
-    </>
+   <div>
+     No user available.
+   </div>
   );
 }
 
