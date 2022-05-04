9527bf067bd99b150309f8427e0d20e9c790db7e
Sara Williams <sara.williams.n@gmail.com>
2022-05-04 15:16:08 -0400

    added links to be able to navigate from page to page

--- src/pages.js
+++ src/pages.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { Link, useLocation } from "react-router-dom";
 
 export function Home() {
     return (
     <div>
         <h1>Company Website</h1>
+        <nav>
+                <Link to="about">About</Link>
+                <Link to="events">Events</Link>
+                <Link to="contact">Contact</Link>
+        </nav>
     </div>
     );
 }
@@ -13,7 +19,7 @@ export function About() {
         <div>
             <h1>About</h1>
         </div>
-        );
+    );
 }
 
 export function Events() {
@@ -30,4 +36,14 @@ export function Contact() {
             <h1>Contact</h1>
         </div>
         );
+}
+
+export function Whoops404() {
+    let location = useLocation();
+    console.log(location);
+    return (
+        <div>
+            <h1>Resource not found at {location.pathname}</h1>
+        </div>
+    )
 }
