e5efcc7d5279b6be62ec38084bf5f8a70561df97
Sara Williams <sara.williams.n@gmail.com>
2022-05-04 15:22:56 -0400

    added useLocation hook

--- src/pages.js
+++ src/pages.js
@@ -40,7 +40,6 @@ export function Contact() {
 
 export function Whoops404() {
     let location = useLocation();
-    console.log(location);
     return (
         <div>
             <h1>Resource not found at {location.pathname}</h1>
